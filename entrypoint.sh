c48773147edd83086cfc625b5a18a4a9f821dea4
SHUBHAM . <Shubham34670@gmail.com>
2024-09-04 21:49:07 +0530

    updated the entrypoint.sh and django-cors-headers in requirements.txt

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,7 +21,7 @@ def create_superuser():
         print('Superuser already exists')
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dev_env.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_ledger.dev_env.settings')
     import django
     django.setup()
     create_superuser()
